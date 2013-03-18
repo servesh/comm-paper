e85d723238d7d84c19cce51b94b38819ebc40626
Servesh Muralidharan <muralis@tcd.ie>
2013-03-18 15:48:29 +0000

    Re commit

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: paper.tex
 	pdflatex -interaction=nonstopmode paper.tex
-	bibtex paper.aux
+	bibtex paper
 	pdflatex -interaction=nonstopmode paper.tex
 	rm -rf *.log *.blg *.bbl
--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 while inotifywait paper.tex; do
 #    pdflatex -interaction=nonstopmode paper.tex
-     make
+#     make
+    pdflatex -interaction=nonstopmode paper.tex
+    bibtex paper
+    pdflatex -interaction=nonstopmode paper.tex
+    pdflatex -interaction=nonstopmode paper.tex
+    rm -rf *.log *.blg *.bbl *.aux
 done
